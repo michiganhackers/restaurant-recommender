2a9ca01fc077849222705924795755c3a14dfdd4
Thomas Smith <thsm@umich.edu>
2021-09-16 19:52:36 -0400

    Add starter iOS code

---
+++ Restaurant Recommender/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restaurant Recommender
+//
+//  Created by Thomas Smith on 9/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
