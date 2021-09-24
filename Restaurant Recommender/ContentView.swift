74b1613c73a81ac3d94c412bd65b631959faadbe
ailijev <ailijev@umich.edu>
2021-09-23 20:30:49 -0400

    Updated home page

--- Restaurant Recommender/ContentView.swift
+++ Restaurant Recommender/ContentView.swift
@@ -23,26 +23,51 @@ struct ContentView_Previews: PreviewProvider {
 
 
 struct Home : View {
+    @State var search = ""
     var body: some View {
         VStack{
             HStack{
-                Text("WolverEats")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding(.leading,25)
                 Spacer()
-                Circle()
+                Text("WolverEats")
+                    .font(.system(size: 30, weight: .semibold))
                     .foregroundColor(Color("maize"))
-                    .frame(width: 40, height: 40)
-                    .overlay(
-                    Image(systemName: "magnifyingglass")
-                        .font(.system(size: 20))
-                    )
-                    .padding(.trailing,20)
-            }.padding(.top, 5)
+                Spacer()
+            }.padding(.top, 50).background(Color("blue"))
+            
+                HStack{
+                    HStack{
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(Color("blue")).font(.system(size: 18, weight: .semibold)).padding(.leading,15)
+                        TextField("Search", text: $search).padding(.horizontal,2).frame(height: UIScreen.main.bounds.height/19, alignment: .center)
+                    }.background(Color("maize")).cornerRadius(25).padding(.leading,15)
+                    
+                    Image(systemName: "line.horizontal.3.decrease.circle")
+                        .foregroundColor(Color("maize"))
+                        .font(.system(size: 33))
+                        .padding(.horizontal,5)
+                        .padding(.trailing, 8)
+                }
+                
+            ScrollView(.vertical, showsIndicators: false){
+                VStack{
+                    HStack{
+                    Text("Restaurant Name").foregroundColor(Color("blue")).font(.title3).fontWeight(.semibold).padding().frame(maxWidth: .infinity, alignment: .leading)
+                    Spacer()
+                    
+                        Image(systemName: "star") //star changes to fill when favorited
+                            .foregroundColor(Color("maize"))
+                            .padding(.trailing)
+                            .font(.system(size:20))
+                    }
+                    Spacer()
+                }.frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height / 6).background(Color("maize").opacity(0.4)).cornerRadius(20).padding(.top,20)
+            }
+            
+            
+            
             
             Spacer()
-        
-        }// blue background
+            
+        }.background(Color("blue")).edgesIgnoringSafeArea(.all)// blue background
     }
 }
