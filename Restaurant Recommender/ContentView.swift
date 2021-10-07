82a38cd30d6dba2ab6f5325b6f8985d9e2eba89b
ailijev <ailijev@umich.edu>
2021-10-07 19:43:33 -0400

    modified home page design

--- Restaurant Recommender/ContentView.swift
+++ Restaurant Recommender/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView_Previews: PreviewProvider {
 
 struct Home : View {
     @State var search = ""
+    var columns = Array(repeating: GridItem(.flexible()), count: 2)
     var body: some View {
         VStack{
             HStack{
@@ -39,6 +40,7 @@ struct Home : View {
                         Image(systemName: "magnifyingglass")
                             .foregroundColor(Color("blue")).font(.system(size: 18, weight: .semibold)).padding(.leading,15)
                         TextField("Search", text: $search).padding(.horizontal,2).frame(height: UIScreen.main.bounds.height/19, alignment: .center)
+                            
                     }.background(Color("maize")).cornerRadius(25).padding(.leading,15)
                     
                     Image(systemName: "line.horizontal.3.decrease.circle")
@@ -47,23 +49,35 @@ struct Home : View {
                         .padding(.horizontal,5)
                         .padding(.trailing, 8)
                 }
-                
+//
+//            ScrollView(.vertical, showsIndicators: false){
+//                VStack{
+//                    HStack{
+//                    Text("Restaurant Name").foregroundColor(Color("blue")).font(.title3).fontWeight(.semibold).padding().frame(maxWidth: .infinity, alignment: .leading)
+//                    Spacer()
+//
+//                        Image(systemName: "star") //star changes to fill when favorited
+//                            .foregroundColor(Color("maize"))
+//                            .padding(.trailing)
+//                            .font(.system(size:20))
+//                    }
+//                    Spacer()
+//                }.frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height / 5).background(Color("maize").opacity(0.4)).cornerRadius(20).padding(.top,20)
+//            }
+            
+            
             ScrollView(.vertical, showsIndicators: false){
-                VStack{
-                    HStack{
-                    Text("Restaurant Name").foregroundColor(Color("blue")).font(.title3).fontWeight(.semibold).padding().frame(maxWidth: .infinity, alignment: .leading)
-                    Spacer()
-                    
-                        Image(systemName: "star") //star changes to fill when favorited
-                            .foregroundColor(Color("maize"))
-                            .padding(.trailing)
-                            .font(.system(size:20))
+                LazyVGrid(columns: columns, spacing: 20){
+                    //change for uppercase and lowercase?
+                    ForEach(mData.filter({"\($0)".contains(search) || search.isEmpty})){ i in
+                        Text(i.title)
+                            .padding(.all, 30)
+                            .foregroundColor(.white)
+                            
                     }
-                    Spacer()
-                }.frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height / 6).background(Color("maize").opacity(0.4)).cornerRadius(20).padding(.top,20)
+                }
+                
             }
-            
-            
             
             
             Spacer()
@@ -71,3 +85,17 @@ struct Home : View {
         }.background(Color("blue")).edgesIgnoringSafeArea(.all)// blue background
     }
 }
+
+
+struct MyData: Identifiable {
+    var id = UUID()
+    var title: String
+}
+
+var mData = [
+
+MyData(title: "Savas"),
+MyData(title: "Stray Hen"),
+MyData(title: "aMa Bistro")
+
+]
