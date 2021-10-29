ea295ce14da007afaf190422043721c918c8f7e0
ailijev <ailijev@umich.edu>
2021-10-28 20:27:32 -0400

    added tab bar

--- Restaurant Recommender/ContentView.swift
+++ Restaurant Recommender/ContentView.swift
@@ -44,63 +44,61 @@ struct Home : View {
                 Spacer()
             }.padding(.top, 50)
             
-//                HStack{
-//                    HStack{
-//                        Image(systemName: "magnifyingglass")
-//                            .foregroundColor(Color("blue")).font(.system(size: 18, weight: .semibold)).padding(.leading,15)
-////                        TextField("Search", text: $search).padding(.horizontal,2).frame(height: UIScreen.main.bounds.height/19, alignment: .center)
-//
-//                    }.background(Color("maize")).cornerRadius(25).padding(.leading,15)
-//
-//                    Image(systemName: "line.horizontal.3.decrease.circle")
-//                        .foregroundColor(Color("maize"))
-//                        .font(.system(size: 33))
-//                        .padding(.horizontal,5)
-//                        .padding(.trailing, 8)
-//                }
-//
-//            ScrollView(.vertical, showsIndicators: false){
-//                VStack{
-//                    HStack{
-//                    Text("Restaurant Name").foregroundColor(Color("blue")).font(.title3).fontWeight(.semibold).padding().frame(maxWidth: .infinity, alignment: .leading)
-//                    Spacer()
-//
-//                        Image(systemName: "star") //star changes to fill when favorited
-//                            .foregroundColor(Color("maize"))
-//                            .padding(.trailing)
-//                            .font(.system(size:20))
-//                    }
-//                    Spacer()
-//                }.frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height / 5).background(Color("maize").opacity(0.4)).cornerRadius(20).padding(.top,20)
-//            }
+            //                HStack{
+            //                    HStack{
+            //                        Image(systemName: "magnifyingglass")
+            //                            .foregroundColor(Color("blue")).font(.system(size: 18, weight: .semibold)).padding(.leading,15)
+            ////                        TextField("Search", text: $search).padding(.horizontal,2).frame(height: UIScreen.main.bounds.height/19, alignment: .center)
+            //
+            //                    }.background(Color("maize")).cornerRadius(25).padding(.leading,15)
+            //
+            //                    Image(systemName: "line.horizontal.3.decrease.circle")
+            //                        .foregroundColor(Color("maize"))
+            //                        .font(.system(size: 33))
+            //                        .padding(.horizontal,5)
+            //                        .padding(.trailing, 8)
+            //                }
+            //
+            //            ScrollView(.vertical, showsIndicators: false){
+            //                VStack{
+            //                    HStack{
+            //                    Text("Restaurant Name").foregroundColor(Color("blue")).font(.title3).fontWeight(.semibold).padding().frame(maxWidth: .infinity, alignment: .leading)
+            //                    Spacer()
+            //
+            //                        Image(systemName: "star") //star changes to fill when favorited
+            //                            .foregroundColor(Color("maize"))
+            //                            .padding(.trailing)
+            //                            .font(.system(size:20))
+            //                    }
+            //                    Spacer()
+            //                }.frame(width: UIScreen.main.bounds.width/1.1, height: UIScreen.main.bounds.height / 5).background(Color("maize").opacity(0.4)).cornerRadius(20).padding(.top,20)
+            //            }
             
             
-//  /Users/angelinailijevski/Desktop/restaurant-recommender/Restaurant Recommender/ModelData.swift          ScrollView(.vertical, showsIndicators: false){
-//                LazyVGrid(columns: columns, spacing: 20){
-//                    //change for uppercase and lowercase?
-//                    ForEach(mData.filter({"\($0)".contains(search) || search.isEmpty})){ i in
-//                        Text(i.title)
-//                            .padding(.all, 30)
-//                            .foregroundColor(.white)
-//
-//                    }
-//                }
-//
-//            }
+            //  /Users/angelinailijevski/Desktop/restaurant-recommender/Restaurant Recommender/ModelData.swift          ScrollView(.vertical, showsIndicators: false){
+            //                LazyVGrid(columns: columns, spacing: 20){
+            //                    //change for uppercase and lowercase?
+            //                    ForEach(mData.filter({"\($0)".contains(search) || search.isEmpty})){ i in
+            //                        Text(i.title)
+            //                            .padding(.all, 30)
+            //                            .foregroundColor(.white)
+            //
+            //                    }
+            //                }
+            //
+            //            }
             HStack{
                 SearchBar(text: $searchItems).offset(y: -8)
                 Image(systemName: "line.horizontal.3.decrease.circle")
-                .foregroundColor(Color("blue"))
-                .font(.system(size: 25))
-                .padding(.trailing, 8)
-                .offset(y: -8)
+                    .foregroundColor(Color("blue"))
+                    .font(.system(size: 25))
+                    .padding(.trailing, 8)
+                    .offset(y: -8)
             }
             List() {
                 ForEach(self.resList.filter({"\($0)".contains(self.searchItems) || self.searchItems.isEmpty})) { r in
-                    Button(action: {
-                        isPresented.toggle()
-                    }){
-                HStack {
+                    
+                    HStack {
                         HStack(spacing: 20) {
                             Image(systemName: "star")
                                 .foregroundColor(.white)
@@ -113,21 +111,62 @@ struct Home : View {
                                     .font(.footnote)
                                     .foregroundColor(.white)
                             }
+                            Spacer()
                         }
-                }.listRowBackground(Color("blue"))
-                    }.sheet(isPresented: $isPresented){
-                        openView()
+                    }.listRowBackground(Color("blue"))
+                        .onTapGesture {
+                            isPresented.toggle()
+                            
+                        }.sheet(isPresented: $isPresented){
+                            openView()
+                        }
+                }
+                
+            }
+            HStack(spacing: 50){
+                
+                Image(systemName: "house.fill")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(Color("blue"))
+                    .frame(width: UIScreen.main.bounds.width/5, height: UIScreen.main.bounds.height/28)
+                    .padding(.top, 10)
+                    .onTapGesture{
+                        //change view to home view
+                        //fill icon
                     }
-            }.offset(y: -16)
+                
+                Image(systemName: "star")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(Color("blue"))
+                    .frame(width: UIScreen.main.bounds.width/5, height: UIScreen.main.bounds.height/28)
+                    .padding(.top, 10)
+                    .onTapGesture {
+                        //change view to favorites
+                        //fill icon
+                    }
+                Image(systemName: "person")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(Color("blue"))
+                    .frame(width: UIScreen.main.bounds.width/5, height: UIScreen.main.bounds.height/28)
+                    .padding(.top, 10)
+                    .onTapGesture {
+                        //change view to person profile
+                        //fill icon
+                        
+                    }
+                
+            }.offset(y: -10).frame(height: UIScreen.main.bounds.height/15).padding(.bottom,5)
             
             
         }.background(Color.white).edgesIgnoringSafeArea(.all)// blue background
-        }
+        
+        
+    }
 }
 
 struct openView : View {
     var body: some View{
-        Text("hello")
+        Text("hello") //replace with detail view
     }
 }
 struct MyData: Identifiable {
@@ -136,9 +175,9 @@ struct MyData: Identifiable {
 }
 
 var mData = [
-
-MyData(title: "Savas"),
-MyData(title: "Stray Hen"),
-MyData(title: "aMa Bistro")
-
+    
+    MyData(title: "Savas"),
+    MyData(title: "Stray Hen"),
+    MyData(title: "aMa Bistro")
+    
 ]
